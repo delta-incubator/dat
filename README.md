505444998876f35c07bb0f19d268c34369aa9347
Matthew Powers <matthewkevinpowers@gmail.com>
2022-08-15 07:11:00 -0400

    First pass at creating reference tables

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# dat
+# dat: Delta Acceptance Testing
+
+Warning: this project is incubating 🚧 🛠.  Any changes are possible in the future, including full deletion of this repo.
+
+This repo contains Delta Lake reference tables.  It specifies functionality that the [Delta connectors](https://github.com/delta-io/connectors) should be able to implement.  The Delta Lake connectors will use these reference tables in integration tests to demonstrate they can perform the requisite read / write operations on Delta Lakes.
+
+These tests will ensure that the various Delta connectors are interoperable and help deliver a wonderful user experience to end users.
+
+## Reference tables
+
+See the `reference_tables` directory for the tables you can access in this project.  Here's the layout of the reference tables directory:
+
+```
+reference_tables
+    reference_table1
+        table
+            _delta_log
+                N.checkpoint
+                N+1.json
+            uuid1.parquet
+            uuid2.parquet
+        table_metadata.json
+        table_content.parquet
+    reference_table2
+        ...
+    reference_table3
+        ...
+```
+
+Here's what each reference table contains:
+
+* `table`: A Delta Lake
+* `table_metadata.json`: metadata about the Delta Lake stored in a JSON file
+* `table_content.parquet`: the "final state" data that should be contained in the Delta Lake
+
+## Setup
+
+Create the conda environment on your machine by running `conda env create -f envs/pyspark-322-delta-200.yml`.
+
+Activate the conda environment with `conda activate pyspark-322-delta-200`.
+
+## Create reference tables
+
+Create the reference tables with `python dat/create_reference_tables.py`.
+
+The reference tables are already checked into this repo, so you don't need to create them yourself.
---
+++ dat/create_reference_tables.py
@@ -0,0 +1,82 @@
+# script is run with the pyspark-322-delta-200 environment
+# create the env with this command: conda env create -f envs/pyspark-322-delta-200
+# activate the environment with this command: conda activate pyspark-322-delta-200
+
+import pyspark
+from delta import *
+
+builder = (
+    pyspark.sql.SparkSession.builder.appName("MyApp")
+    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+    .config(
+        "spark.sql.catalog.spark_catalog",
+        "org.apache.spark.sql.delta.catalog.DeltaCatalog",
+    )
+)
+
+spark = configure_spark_with_delta_pip(builder).getOrCreate()
+
+
+def create_reference_table1():
+    columns = ["letter", "number", "a_float"]
+    data = [("a", 1, 1.1), ("b", 2, 2.2), ("c", 3, 3.3)]
+    rdd = spark.sparkContext.parallelize(data)
+    df = rdd.toDF(columns)
+
+    df.write.format("delta").save("reference_tables/reference_table1/table")
+
+    columns = ["letter", "number", "a_float"]
+    data = [("d", 4, 4.4), ("e", 5, 5.5)]
+    rdd = spark.sparkContext.parallelize(data)
+    df = rdd.toDF(columns)
+
+    df.write.mode("append").format("delta").save(
+        "reference_tables/reference_table1/table"
+    )
+
+    df = spark.read.format("delta").load("reference_tables/reference_table1/table")
+
+    df.toPandas().to_parquet(
+        "reference_tables/reference_table1/table_content.parquet"
+    )
+
+    # TODO: Create the table_metadata.json file
+
+    # Structure of table_metadata.json
+    #     table name: String
+    #     reader_protocol_version: integer
+    #     writer_protocol_version: integer
+    #     (optional) schema: Nested json
+    #     (optional) features: array-of-string
+
+
+def create_reference_table2():
+    columns = ["letter", "number", "a_float"]
+    data = [("a", 1, 1.1), ("a", 2, 2.2), ("c", 3, 3.3)]
+    rdd = spark.sparkContext.parallelize(data)
+    df = rdd.toDF(columns)
+
+    df.write.partitionBy("letter").format("delta").save(
+        "reference_tables/reference_table2/table"
+    )
+
+    columns = ["letter", "number", "a_float"]
+    data = [("a", 4, 4.4), ("e", 5, 5.5)]
+    rdd = spark.sparkContext.parallelize(data)
+    df = rdd.toDF(columns)
+
+    df.write.partitionBy("letter").mode("append").format("delta").save(
+        "reference_tables/reference_table2/table"
+    )
+
+    df = spark.read.format("delta").load("reference_tables/reference_table2/table")
+
+    df.toPandas().to_parquet(
+        "reference_tables/reference_table2/table_content.parquet"
+    )
+
+    # TODO: Create the table_metadata.json file
+
+
+create_reference_table1()
+create_reference_table2()
